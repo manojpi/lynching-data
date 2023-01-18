151d81b0676e650914403f45f926e818870a1ed0
manojpi <111719118+manojpi@users.noreply.github.com>
2023-01-18 11:20:58 -0600

    Add files via upload
    This the data structured into excel and csv...also removed some faulty data such as double name (eg: Manoj or Pratyush) for a single record

---
+++ chapter_9/data_processing_9.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import csv
+
+crime_to_data = {}
+crime_stack = []
+
+def structure_raw_data():
+
+    with open("raw_data_9.txt", "rt") as file:
+        for line in file:
+            if not line.isspace():
+                if line.isupper():
+                    line = line.strip("\n")
+                    crime_stack.append(line.capitalize())
+                    crime_to_data[crime_stack[-1]] = []
+                    continue
+                
+
+                for crime in line.split("; "):
+                    crime = crime.strip("\n")
+                    crime = crime.split(", ")
+                    for index in range(len(crime)):
+                            
+                        crime[index] = crime[index].strip()
+                        if "——" in crime[index]:
+                            crime[index] = crime[index].replace("——", "")
+
+                    crime_to_data[crime_stack[-1]].append(crime)
+                    crime.append(crime_stack[-1])
+                    crime_to_data[crime_stack[-1]].append(crime)
+
+
+def create_csv():
+
+    with open("processed_data_9.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date","Name","City/County","State","Crime"])
+
+        for crime in crime_to_data:
+            for data in crime_to_data[crime]:
+                writer.writerow(data)
+
+
+def create_excel():
+
+    read_file = pd.read_csv("processed_data_9.csv")
+    read_file.to_excel("processed_data_2.xlsx", header = True, index = None)
+
+if __name__ == "__main__":
+    structure_raw_data()
+    create_csv()
+    create_excel()
