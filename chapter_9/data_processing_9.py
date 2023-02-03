5e414034f58ca26f5f34f7d9105583408aae701a
manojpi <111719118+manojpi@users.noreply.github.com>
2023-02-02 20:31:50 -0600

    Add files via upload

--- chapter_9/data_processing_9.py
+++ chapter_9/data_processing_9.py
@@ -15,26 +15,29 @@ def structure_raw_data():
                     crime_to_data[crime_stack[-1]] = []
                     continue
                 
-
                 for crime in line.split("; "):
                     crime = crime.strip("\n")
                     crime = crime.split(", ")
                     for index in range(len(crime)):
+
+                        if index == 1:
+                            crime.extend(crime[index].split(maxsplit=1))
                             
                         crime[index] = crime[index].strip()
-                        if "——" in crime[index]:
-                            crime[index] = crime[index].replace("——", "")
+                    
+                    crime.pop(1)
+                    crime.insert(0,crime_stack[-1])
+                    crime.insert(0, len(crime_stack))
+                    crime.append(".")
+                    crime_to_data[crime_stack[-1]].append(crime)
 
-                    crime_to_data[crime_stack[-1]].append(crime)
-                    crime.append(crime_stack[-1])
-                    crime_to_data[crime_stack[-1]].append(crime)
 
 
 def create_csv():
 
     with open("processed_data_9.csv", "w") as file:
         writer = csv.writer(file)
-        writer.writerow(["Date","Name","Location","State","Crime"])
+        writer.writerow(["Charge_ID", "Charge", "Date", "City", "State" , "First_Name", "Last_Name", "Gender"])
 
         for crime in crime_to_data:
             for data in crime_to_data[crime]:
@@ -44,7 +47,7 @@ def create_csv():
 def create_excel():
 
     read_file = pd.read_csv("processed_data_9.csv")
-    read_file.to_excel("processed_data_2.xlsx", header = True, index = None)
+    read_file.to_excel("processed_data_9.xlsx", header = True, index = None)
 
 if __name__ == "__main__":
     structure_raw_data()
